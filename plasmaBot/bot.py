c5a7860c2deac3bac8a979fe16392c072a06a160
A Detection Algorithm (ADA) <plasma2403@gmail.com>
2016-05-19 11:17:56 -0700

    Reset to original, test for working

--- plasmaBot/bot.py
+++ plasmaBot/bot.py
@@ -1870,43 +1870,28 @@ class PlasmaBot(discord.Client):
 
     async def on_message(self, message):
         await self.wait_until_ready()
-        
+
         message_content = message.content.strip()
-        
-        #if message.author == self.user:
-        #    self.safe_print("Ignoring message from myself (%s)" % message.content)
-        #    return
-        
-        self.safe_print('t')
-        
-        if self.config.bound_channels and message.channel.id not in self.config.bound_channels and not message.channel.is_private:
-            return
-        
-        self.safe_print('stage2')
-        
         if not message_content.startswith(self.config.command_prefix):
-            messageType = auto
-            message_content = message_content.replace(" ", "_")
-            command, *args = message_content.split()
-            command = command.lower().strip()
+            return
 
-        else:
-            messageType = cmd
-            command, *args = message_content.split()
-            command = command[len(self.config.command_prefix):].lower().strip()
+        if message.author == self.user:
+            self.safe_print("Ignoring command from myself (%s)" % message.content)
+            return
 
-        handler = getattr(self, '%s_%s' % (messageType, command), None)
-        self.safe_print("%s_%s" % (messageType, command))
+        if self.config.bound_channels and message.channel.id not in self.config.bound_channels and not message.channel.is_private:
+            return  # if I want to log this I just move it under the prefix check
+
+        command, *args = message_content.split()  # Uh, doesn't this break prefixes with spaces in them (it doesn't, config parser already breaks them)
+        command = command[len(self.config.command_prefix):].lower().strip()
+
+        handler = getattr(self, 'cmd_%s' % command, None)
         if not handler:
             return
 
         if message.channel.is_private:
-            if messageType == 'cmd' and not (message.author.id == self.config.owner_id and (command == 'joinserver' or command == 'invite')):
-                await self.send_message(message.channel, 'PlasmaBot does not respond in Private Messages.')
-                return
-
-            if messageType == 'auto' and not (message.author.id == self.config.owner_id and command == 'watergame'):
-                await self.send_message(message.channel, 'PlasmaBot does not respond in Private Messages.')
+            if not (message.author.id == self.config.owner_id and command == 'joinserver'):
+                await self.send_message(message.channel, 'You cannot use this bot in private messages.')
                 return
 
         if message.author.id in self.blacklist and message.author.id != self.config.owner_id:
@@ -1954,39 +1939,32 @@ class PlasmaBot(discord.Client):
             if params.pop('voice_channel', None):
                 handler_kwargs['voice_channel'] = message.server.me.voice_channel
 
-            if params.pop('leftover_args', None) and messageType == 'cmd':
+            if params.pop('leftover_args', None):
                 handler_kwargs['leftover_args'] = args
 
-            if messageType == 'cmd':
-                args_expected = []
-                for key, param in list(params.items()):
-                    doc_key = '[%s=%s]' % (key, param.default) if param.default is not inspect.Parameter.empty else key
-                    args_expected.append(doc_key)
+            args_expected = []
+            for key, param in list(params.items()):
+                doc_key = '[%s=%s]' % (key, param.default) if param.default is not inspect.Parameter.empty else key
+                args_expected.append(doc_key)
 
-                    if not args and param.default is not inspect.Parameter.empty:
-                        params.pop(key)
-                        continue
+                if not args and param.default is not inspect.Parameter.empty:
+                    params.pop(key)
+                    continue
 
-                    if args:
-                        arg_value = args.pop(0)
-                        handler_kwargs[key] = arg_value
-                        params.pop(key)
-
-            if messageType == 'cmd':
-                messageTypeWritten = 'command'
-
-            if messageType == 'auto':
-                messageTypeWritten = 'autoreply'
+                if args:
+                    arg_value = args.pop(0)
+                    handler_kwargs[key] = arg_value
+                    params.pop(key)
 
             if message.author.id != self.config.owner_id:
                 if user_permissions.command_whitelist and command not in user_permissions.command_whitelist:
                     raise exceptions.PermissionsError(
-                        "This %s is not enabled for your group (%s)." % (messageTypeWritten, user_permissions.name),
+                        "This command is not enabled for your group (%s)." % user_permissions.name,
                         expire_in=20)
 
                 elif user_permissions.command_blacklist and command in user_permissions.command_blacklist:
                     raise exceptions.PermissionsError(
-                        "This %s is disabled for your group (%s)." % (messageTypeWritten, user_permissions.name),
+                        "This command is disabled for your group (%s)." % user_permissions.name,
                         expire_in=20)
 
             if params:
@@ -2030,7 +2008,7 @@ class PlasmaBot(discord.Client):
                 expire_in=expirein,
                 also_delete=alsodelete
             )
-
+ 
         except exceptions.Signal:
             raise
 
