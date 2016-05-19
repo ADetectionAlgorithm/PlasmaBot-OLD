3f51c4d4dbd5ec6f6a7acf82c2de75bb894247be
A Detection Algorithm (ADA) <plasma2403@gmail.com>
2016-05-19 08:23:30 -0700

    Updated VersionInfo

--- plasmaBot/constants.py
+++ plasmaBot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
-MAIN_VERSION = '1.9.5'
-SUB_VERSION = '-review-2016.5.15'
+MAIN_VERSION = '0.1.1'
+SUB_VERSION = '-beta-2016.5.19'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
